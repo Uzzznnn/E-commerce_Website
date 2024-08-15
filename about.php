05cb59c39c53aace2d0374777cc1ba071466b687
Uzzznnn <020bim063@sxc.edu.np>
2024-08-15 19:41:19 +0545

    Zen website

---
+++ about.php
@@ -0,0 +1,134 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" /> 
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/Zen.png" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Hi! ZenFamily</h3>
+         <p>Hey Ujen Tuladhar here! Founder of Zen&Kool and just wanted to say you're awesome and be yourself, be different and potray yourself more through us! Thankyou and congrats on being the zenerations koolest *wink* *wink* .</p>
+
+         <p>I would like to thank <a href="https://www.facebook.com/sxcmaitighar" target="_blank">St. Xaviers College</a> for guiding me through the process and making me able to develop a fun and needed project like this. </p>
+         <a href="contact.php" class="btn">Contact Us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+   
+   <h1 class="heading">Client's Reviews.</h1>
+
+   <div class="swiper reviews-slider">
+
+   <div class="swiper-wrapper"> 
+
+      <div class="swiper-slide slide">
+         <img src="images/pro.png" alt="">
+         <p>These stickers are litteraly the best thankyou!💗</p>
+         <h3><a href="https://www.instagram.com/yourchickenugget/" target="_blank">Yourchickennugget🖤</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pro.png" alt="">
+         <p>Thankyou for the best premium stickers.</p>
+         <h3><a href="https://www.instagram.com/yam_suryam_/"  target="_blank">Yam_suryam_</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pro.png" alt="">
+         <p>Thankyou for the stickers they are very cool❤️.</p>
+         <h3> <a href="https://www.instagram.com/_dipamp/" target="_blank">Dipam Pradhan</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pro.png" alt="">
+         <p>Future ma pani contact garchu yekdamai maanparyo❤️❤️.</p>
+         <h3><a href="https://www.instagram.com/haileyy1658/" target="_blank">Haileyy1658</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pro.png" alt="">
+         <p>It is premium and i'd be interested for future references and orders❤️.</p>
+         <h3><a href="https://www.instagram.com/mr.prawal/" target="_blank">Prawal Pandey</a></h3>
+      </div>
+
+
+   </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Send us a hi @.</h3>
+         <a href="tel:9860855930"><i class="fas fa-phone"></i> +977 9860855930</a>
+         <a href="mailto:ZenNKool@gmail.com"><i class="fas fa-envelope"></i>zennkool@gmail.com</a>
+         <a href="https://www.google.com/maps/@27.7191275,85.3073786,16z?entry=ttu"><i class="fas fa-map-marker-alt"></i> Kathmandu, Nepal </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow for more of us!</h3>
+         <a href="https://www.facebook.com/LEO.UJEN.JR.11/" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://www.instagram.com/ZenNKool/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://www.linkedin.com/in/ujen-tuladhar-457b1b262//" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+</footer>
